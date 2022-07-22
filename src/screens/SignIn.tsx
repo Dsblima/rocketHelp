e989a48e455b0c0634bd1661a8e6256ee81d0805
Dsblima <dsbl@ecomp.poli.br>
2022-07-22 19:11:18 -0300

    Implement Sign in feature

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import {useState} from 'react'
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -9,13 +11,36 @@ import { Button } from '../components/Button';
  
 export function SignIn() {
 
-  const [name, setName] = useState(''); 
+  const [email, setEmail] = useState(''); 
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log(name, password)
+    if (!email || !password) {
+      return Alert.alert('Entrar', 'Informe email e senha');
+    }
+
+    setIsLoading(true);
+
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(response => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+
+      if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password')
+        return Alert.alert('Entrar', 'Email ou  senha inválido,');
+      if (error.code === 'auth/invalid-email')
+        return Alert.alert('Entrar', 'Email inválido');
+
+      return Alert.alert('Entrar', 'Não foi possível realizar o login.')
+      
+    });
   }
 
   return(
@@ -31,7 +56,7 @@ export function SignIn() {
         placeholder="Email"
         mb={4}
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300] }/>} ml={4} />}
-        onChangeText={setName}
+        onChangeText={setEmail}
         />
 
       <Input 
@@ -42,7 +67,12 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title='Entrar' w="full" onPress={handleSignIn}/>
+      <Button 
+        title='Entrar' 
+        w="full" 
+        onPress={handleSignIn}
+        isLoading={isLoading}  
+      />
     </VStack>
   )
 }
